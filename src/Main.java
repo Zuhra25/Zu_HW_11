1abd956efbd272228e817b63bffa966aef330fd1
ZU <zshaihova@gmail.com>
2023-05-04 21:29:52 +0300

    задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        task1();
+        //task1();
+        //task2();
+        task3();
     }
     public static void checkLeapYear(int checkYear) {
         boolean leapYear = false;
@@ -15,21 +17,49 @@ public class Main {
             System.out.println(checkYear + " год НЕ является високосным");
     }
 
-//    public static void printYear(boolean leapYear) {
-//        if (leapYear) {
-//            System.out.println("год является високосным");
-//        } else {
-//            System.out.println("год НЕ является високосным");
-//        }
-//    }
-
     public static void task1() {
-        System.out.println("Задача 1");
+        System.out.println("Задача 1:");
         //Реализуйте метод, который получает в качестве параметра год,
         // проверяет, является ли он високосным, и выводит результат в консоль.
 
         int year = 2100;
         checkLeapYear(year);
         //printYear();
+
+    }
+
+    public static void osAndYear(int os, int year) {
+        int clientDeviceYear = 2015;
+        switch (os) {
+            case 0:
+                if (year <= clientDeviceYear) {
+                    System.out.println("Установите ОБЛЕГЧЕННУЮ версию приложения для iOS по ссылке");
+                    break;
+                } else {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                    break;
+                }
+            case 1:
+                if (year <= clientDeviceYear) {
+                    System.out.println("Установите ОБЛЕГЧЕННУЮ версию приложения для Android по ссылке");
+                    break;
+                } else {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                    break;
+                }
+        }
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2:");
+
+        int clientOS = 0;
+        int clientDevice = 2000;
+        osAndYear(clientOS, clientDevice);
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3: ");
+
     }
 }
